e26d8f7900b6073518daed1b2f49c5af9f1badcf
sava <savamedvedev@gmail.com>
2022-06-22 14:30:18 +0800

    four-commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { validationResult } from "express-validator";
 
+//////////////////////////////////////////////////////////////////////////
 import { registerValidator } from "./validations/auth.js";
-
 import UserModel from "./models/User.js";
 
 const username = encodeURIComponent("admin");
@@ -21,6 +22,41 @@ const app = express();
 
 app.use(express.json());
 
+app.post("/auth/login", async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+    if (!isValidPass) {
+      return res.status(404).json({ message: "Login and Password not valid" });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Не удалось авторизоватся",
+    });
+  }
+});
+
 app.post("/auth/register", registerValidator, async (req, res) => {
   try {
     const errors = validationResult(req);
@@ -29,18 +65,30 @@ app.post("/auth/register", registerValidator, async (req, res) => {
     }
     const password = req.body.password;
     const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
+    const hash = await bcrypt.hash(password, salt);
 
     const doc = new UserModel({
       email: req.body.email,
       fullName: req.body.fullName,
       avatarUrl: req.body.avatarUrl,
-      passwordHash,
+      passwordHash: hash,
     });
 
     const user = await doc.save();
 
-    res.json(user);
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
   } catch (error) {
     console.log(error);
     res.status(500).json({
